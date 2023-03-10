8ab1040e3a47084149e7dfdfbf421ee66b140b6b
Rida-karim <123716596+Rida-karim@users.noreply.github.com>
2023-03-10 21:39:22 +0500

    Create Reverse_String.sh

---
+++ PRACTICE_QUESTION/Reverse_String.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter string:" 
+read string
+
+len=${#string}
+
+for (( i=$len-1; i>=0; i-- ))
+do
+# "${string:$i:1}"extract single single character from string.
+reverse="$reverse${string:$i:1}"
+done
+
+echo "$reverse"
